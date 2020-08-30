7ab9df669a8aa6adcef4707149b66f8f69d91c5d
Denis Cheremisov <denis.cheremisov@gmail.com>
2020-08-30 14:01:50 +0300

    feat: helpers for getting input and output msgs

--- ast/method.go
+++ ast/method.go
@@ -38,3 +38,22 @@ type MethodOptionValue struct {
 	Name  string
 	Value string
 }
+
+// InputMessage возвращает структуру запроса (минуя оборачивающий Stream, если нужно)
+func (m *Method) InputMessage() *Message {
+	return getMessage(m.Input)
+}
+
+// OutputMessage аналогично InputMessage, возвращает структуру ответа, при необходимости снимая stream
+func (m *Method) OutputMessage() *Message {
+	return getMessage(m.Output)
+}
+
+func getMessage(m Type) *Message {
+	v, ok := m.(*Message)
+	if ok {
+		return v
+	}
+
+	return m.(*Stream).Type.(*Message)
+}
