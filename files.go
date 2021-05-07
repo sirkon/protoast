540eb3c815b33dcadeebc19ff690faa281c988f0
Denis Cheremisov <denis.cheremisov@gmail.com>
2021-05-07 17:04:00 +0300

    fix: cosmetics

--- files.go
+++ files.go
@@ -14,6 +14,8 @@ type Files interface {
 }
 
 // NewFiles отдаёт реализацию Files построенную на готовом соответствии
+//
+// Deprecated: сущность имеет малый смысл, только хорошее название забрала
 func NewFiles(mapping map[string]string) Files {
 	return &files{
 		mapping: mapping,
@@ -24,6 +26,7 @@ type files struct {
 	mapping map[string]string
 }
 
+// Abs для реализации Files
 func (f *files) Abs(name string) (string, error) {
 	res, ok := f.mapping[name]
 	if !ok {
@@ -32,6 +35,7 @@ func (f *files) Abs(name string) (string, error) {
 	return res, nil
 }
 
+// File для реализации Files
 func (f *files) File(path string) ([]byte, error) {
 	absPath, ok := f.mapping[path]
 	if !ok {
@@ -53,6 +57,7 @@ func NewFilesViaResolver(resolver func(string) (string, error)) Files {
 
 type backResolver func(name string) (string, error)
 
+// File для реализации Files
 func (b backResolver) File(path string) ([]byte, error) {
 	absPath, err := b.Abs(path)
 	if err != nil {
@@ -65,10 +70,11 @@ func (b backResolver) File(path string) ([]byte, error) {
 	return data, nil
 }
 
+// Abs для реализации Files
 func (b backResolver) Abs(path string) (string, error) {
 	res, err := b(path)
 	if err != nil {
-		return "", errors.Wrap(err, "resole path")
+		return "", errors.Wrap(err, "resolve path")
 	}
 	absPath, err := filepath.Abs(res)
 	if err != nil {
